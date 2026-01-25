3a84a74d5ca03a3a9fa6cdfe014a19b5db74cef7
Ata <atabadrbarzegar@gmail.com>
2026-01-25 18:14:33 +0200

    Add better error handling massages to profile.validator.js

--- src/validators/profile.validator.js
+++ src/validators/profile.validator.js
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import {z} from "zod";
 
 export const getProfileSchema = z.object({
     body: z.object({}).optional(),
@@ -8,13 +8,31 @@ export const getProfileSchema = z.object({
 
 export const updateProfileSchema = z.object({
     body: z.object({
-        firstName: z.string().min(1, "First name is required."),
-        familyName: z.string().min(1, "Family name is required."),
-        address: z.string().min(1, "Address is required."),
-        postalCode: z.string().min(1, "Postal code is required."),
-        city: z.string().min(1, "City is required."),
+        firstName: z
+            .string({
+                error: issue => issue.input === undefined ? "First Name is Required" : "This field must be a string"
+            }),
+        familyName: z
+            .string({
+                error: issue => issue.input === undefined ? "Family Name is Required" : "This field must be a string"
+            }),
+        address: z
+            .string({
+                error: issue => issue.input === undefined ? "Address is Required" : "This field must be a string"
+            }),
+        postalCode: z
+            .string({
+                error: issue => issue.input === undefined ? "Postal code is Required" : "This field must be a string"
+            }),
+        city: z
+            .string({
+                error: issue => issue.input === undefined ? "City Name is Required" : "This field must be a string"
+            }),
         phoneNumber: z
-            .string()
+            .string(
+                {
+                    error: issue => issue.input === undefined ? "Phone Number is Required" : "This field must be a string"
+                })
             .regex(/^\+[1-9]\d{7,14}$/, "Phone number must be in international E.164 format (e.g., +491234567890).")
     }),
     params: z.object({}).optional(),
