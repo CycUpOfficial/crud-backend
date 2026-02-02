96c6871134a472b17296642bfb866aff2f63c83d
Ata <atabadrbarzegar@gmail.com>
2026-02-02 20:07:39 +0200

    Refactor image saving algorithm.

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/async-handler.js";
 import { validateRequest } from "../middlewares/validate.middleware.js";
-import { uploadProfileImage } from "../middlewares/upload.middleware.js";
-import { getProfileSchema, updateProfileSchema } from "../validators/profile.validator.js";
+import { getProfileSchema } from "../validators/profile.validator.js";
 import { getProfile, updateProfile } from "../controllers/profile.controller.js";
 
 const router = Router();
@@ -10,8 +9,6 @@ const router = Router();
 router.get("/users/profile", validateRequest(getProfileSchema), asyncHandler(getProfile));
 router.put(
     "/users/profile",
-    uploadProfileImage,
-    validateRequest(updateProfileSchema),
     asyncHandler(updateProfile)
 );
 
