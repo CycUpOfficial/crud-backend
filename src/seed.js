0342c982b31ea07bd853aedc71884d123e9f6d8e
Ata <atabadrbarzegar@gmail.com>
2026-02-12 00:08:10 +0200

    Add a fake seeder to the system.

--- src/seed.js
+++ src/seed.js
@@ -1,10 +1,14 @@
 import { seedCategories } from "./seeders/categories.seeder.js";
 import { seedCities } from "./seeders/cities.seeder.js";
+import { seedFakeData } from "./seeders/fake.seeder.js";
 
 const runSeed = async () => {
     try {
         await seedCategories();
         await seedCities();
+        if(process.env.NODE_ENV === "development"){
+            await seedFakeData();
+        }
         console.log("Seeding completed successfully.");
         process.exit(0);
     } catch (error) {
